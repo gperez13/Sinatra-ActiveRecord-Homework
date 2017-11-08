4818eb52b59d2378e5d53c8be17a8b6377e69441
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2017-11-07 19:48:33 -0600

    Initial Commit

---
+++ controllers/ApplicationController.rb
@@ -0,0 +1,18 @@
+class ApplicationController < Sinatra::Base
+
+	require 'bundler'
+	Bundler.require
+
+
+
+
+
+
+	set :views, File.expand_path('../../views', __FILE__)
+
+
+	not_found do
+		erb :not_found
+	end
+
+end
